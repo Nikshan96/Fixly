a3e884c12209ea6d425b85f276a0d66565fac4ec
Nikshan Gautam <gautam.nikshan96@gmail.com>
2026-05-16 22:30:16 +0545

    Feat(backend): Add standardized logger utility for console outputs

---
+++ backend/utils/logger.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Simple centralized logger utility
+ */
+const logger = {
+    info: (message, meta = {}) => {
+        console.log([INFO]  - , Object.keys(meta).length ? meta : '');
+    },
+    error: (message, error) => {
+        console.error([ERROR]  - , error || '');
+    },
+    warn: (message, meta = {}) => {
+        console.warn([WARN]  - , Object.keys(meta).length ? meta : '');
+    }
+}
+module.exports = logger;
