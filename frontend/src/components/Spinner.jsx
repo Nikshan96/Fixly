10e607ada3cac161be776e4b5288ebc112a3fb8f
Nikshan Gautam <gautam.nikshan96@gmail.com>
2026-05-16 22:30:10 +0545

    Feat(frontend): Create generic Spinner UI component for loading states

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+
+const Spinner = ({ size = 'medium', color = 'text-blue-500' }) => {
+    const sizeClasses = {
+        small: 'w-4 h-4',
+        medium: 'w-8 h-8',
+        large: 'w-12 h-12'
+    };
+    
+    return (
+        <div className={lex justify-center items-center}>
+            <div className={nimate-spin rounded-full border-b-2 border-current  }></div>
+        </div>
+    );
+};
+
+export default Spinner;
